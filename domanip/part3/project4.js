f03cf227a21dfeb33d1c54ec8f898baa773f3e12
Bayelaye313 <bayelaye0510@gmail.com>
2024-01-06 23:01:54 +0000

    adjust p4

--- domanip/part3/project4.js
+++ domanip/part3/project4.js
@@ -1,64 +1,67 @@
 // Variables for buttons
-const startStopButton = document.getElementById('start-btn');
-const resetButton = document.getElementById('reset-btn');
+const buttonPausePlay = document.querySelector('#start-btn');
+const buttonReset = document.querySelector('#reset-btn');
+
 
 // Variables timers
 const timer = document.querySelector('#timer');
-let seconds = 0;
-let minutes = 0;
 let hours = 0;
-let intervalId; // To store the interval ID
-let isRunning = false;
+let minutes = 0;
+let seconds = 0;
+let intervalId;
+let isrunning = false;
 
 // Fonction de démarrage du chronomètre
-function startWatch() {
-    intervalId = setInterval(stopWatch, 1000); // Appelle stopWatch toutes les 1000 ms (1 seconde)
+function startWatch()
+{
+    intervalId = setInterval(stopWatch, 1000);
 }
 
 // Fonction d'arrêt du chronomètre
 function stopWatch() {
     seconds++;
-    if (seconds / 60 === 1) {
+
+    if (seconds/60 === 1) {
         seconds = 0;
         minutes++;
-        if (minutes / 60 === 1) {
+        if (minutes/60 === 1) {
             minutes = 0;
             hours++;
-            if (hours / 24 === 1) {
-                hours = 0;
+            if (hours/24 === 1) {
+                hours = 0
             }
         }
+    
     }
 
-    let displayTimes = document.querySelector('#timer').innerText =
-        (hours < 10 ? '0' + hours.toString() : hours) + ':' +
-        (minutes < 10 ? '0' + minutes.toString() : minutes) + ':' +
-        (seconds < 10 ? '0' + seconds.toString() : seconds);
-}
+    let displayWatch = timer.innerText = 
+    (hours < 10 ? '0'+ hours : hours) + ':' +
+    (minutes < 10 ? '0'+ minutes : minutes) + ':' +
+    (seconds < 10 ? '0'+ seconds : seconds);
 
+}
 // Fonction toggle status
 function startStopWatch() {
-    if (isRunning) {
+    if (isrunning) {
         clearInterval(intervalId);
-        isRunning = false;
-        document.getElementById('start-btn').innerHTML = '<i class="bx bx-play" id="play"></i>';
+        isrunning = false;
+        buttonPausePlay.innerHTML = '<i class="bx bx-play" id="play"></i>';
     } else {
         startWatch();
-        isRunning = true;
-        document.getElementById('start-btn').innerHTML = '<i class="bx bx-stop" id="stop"></i>';
+        isrunning = true;
+        buttonPausePlay.innerHTML = '<i class="bx bx-stop" id="stop"></i>';
     }
 }
-
 // Fonction de réinitialisation du chronomètre
 function resetWatch() {
     clearInterval(intervalId);
-    seconds = 0;
-    minutes = 0;
     hours = 0;
+    minutes = 0;
+    seconds = 0;
     timer.innerText = '00:00:00';
-    isRunning = false;
+    isrunning = false;
 }
 
 // Écouteurs d'événements pour les boutons
-startStopButton.addEventListener('click', startStopWatch);
-resetButton.addEventListener('click', resetWatch);
+buttonPausePlay.addEventListener('click', startStopWatch);
+buttonReset.addEventListener('click', resetWatch)
